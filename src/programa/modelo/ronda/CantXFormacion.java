3f43202bdd03c6ae361af339b51335dc29f52400
Neielmonti <neielmonti@gmail.com>
2022-12-21 10:52:33 -0300

    La clase CantXFormacion fue cambiada
    Ahora ya no es un record, simplemente es una clase con atributos final y públicos

--- src/programa/modelo/ronda/CantXFormacion.java
+++ src/programa/modelo/ronda/CantXFormacion.java
@@ -1,7 +1,13 @@
 package programa.modelo.ronda;
 import programa.modelo.commons.Formacion;
 import java.io.Serializable;
-public record CantXFormacion(Formacion forma, int cantidad) implements Serializable {
+public class CantXFormacion implements Serializable {
+    public final Formacion forma;
+    public final int cantidad;
+    public CantXFormacion(Formacion forma, int cantidad) {
+        this.forma = forma;
+        this.cantidad = cantidad;
+    }
     public String getString() {
         return (cantidad + " " + forma.getLabel() + "/s");
     }
--- src/programa/modelo/ronda/Ronda.java
+++ src/programa/modelo/ronda/Ronda.java
@@ -15,7 +15,7 @@ public class Ronda implements IRonda, Serializable{
     }
     public Ronda(List<CantXFormacion> formaciones) {
         while (!formaciones.isEmpty()) {
-            Formacion formaActual = formaciones.get(0).forma();
+            Formacion formaActual = formaciones.get(0).forma;
             this.formaciones.add(formaciones.get(0));
             formaciones.remove(0);
             // Se eliminan las formaciones repetidas para evitar el mal uso de las rondas (solo una precaucion)
@@ -25,7 +25,7 @@ public class Ronda implements IRonda, Serializable{
     private void eliminarFormacionesRepetidas(Formacion forma, List<CantXFormacion> formaciones) {
         int i = 0;
         while (i < formaciones.size()) {
-            if (formaciones.get(i).forma() == forma) {
+            if (formaciones.get(i).forma == forma) {
                 formaciones.remove(i);
             }
             else i++;
@@ -37,8 +37,8 @@ public class Ronda implements IRonda, Serializable{
         // Se revisa si el jugador tiene las jugadas (y la cantidad apropiada de ellas) correspondientes a
         // las formaciones
         for (CantXFormacion cf:this.formaciones) {
-            Formacion formaActual = cf.forma();
-            int cantActual = cf.cantidad();
+            Formacion formaActual = cf.forma;
+            int cantActual = cf.cantidad;
             int contador = 0;
             for (Jugada jugada:jugadas) {
                 if ((jugada.getForma() == formaActual) || (formaActual.esEquivalente(jugada.getForma()))) {
